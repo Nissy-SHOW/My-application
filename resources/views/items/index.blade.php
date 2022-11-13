fc793646b98ad82288a6f31870f905be7b396799
Nissy-SHOW <syoo23452345@gmail.com>
2022-11-13 17:29:50 +0900

    修正

--- resources/views/items/index.blade.php
+++ resources/views/items/index.blade.php
@@ -1,1 +1,38 @@
-<h1>テスト</h1>
+<!DOCTYPE html>
+
+    <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+        <head>
+            <meta charset="utf-8">
+            <title>Items</title>
+            <!-- Fonts -->
+            <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+        </head>
+        <body>
+            <div class='items'>
+                @foreach ($items as $item)
+                    <div class='item'>
+                        <a href="/items/{{ $item->id }}" id="{{ $item->id }}" method="get">{{$item->name}}</a>
+                        <p>
+                            <a>{{ $item->content }}</a>
+                        </p>
+                        <form action="/items/{{ $item->id }}" id="form_{{ $item->id }}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button type="button" onclick="deletePost({{ $item->id }})">削除</button>
+                        </form>   
+                    </div>
+                @endforeach
+            </div>
+                <a href='/'>user</a>
+                <a href='/items/create'>createItem</a>
+            <script>
+                function　deletePost(id){
+                    'use strict'
+                    
+                    if(confirm('削除すると復元できません。\n本当に削除しますか?')){
+                        document.getElementById(`form_${id}`).submit();
+                    }
+                }
+            </script>
+        </body>
+    </html>
